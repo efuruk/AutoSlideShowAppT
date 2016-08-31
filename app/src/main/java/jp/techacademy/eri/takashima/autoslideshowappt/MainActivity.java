d7971435b63a03443d1d725f2e60957edba55a9c
Eri Takashima <efuruk@gmail.com>
2016-08-31 07:07:12 -0500

    Initial commit

---
+++ app/src/main/java/jp/techacademy/eri/takashima/autoslideshowappt/MainActivity.java
@@ -0,0 +1,46 @@
+package jp.techacademy.eri.takashima.autoslideshowappt;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.ContentUris;
+import android.view.View;
+import android.webkit.PermissionRequest;
+import android.widget.Button;
+import android.content.Intent;
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageView;
+import java.security.Permissions;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Handler;
+import android.view.Menu;
+import android.widget.TextView;
+import android.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build)
+
+
+    }
+
+
+
+
+}
