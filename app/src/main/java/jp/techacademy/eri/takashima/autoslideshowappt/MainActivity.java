ce5144d3a49297232fdec626dd29c158ae0e9ff9
Eri Takashima <efuruk@gmail.com>
2016-08-31 07:39:04 -0500

    消えた ところの書き足し。

--- app/src/main/java/jp/techacademy/eri/takashima/autoslideshowappt/MainActivity.java
+++ app/src/main/java/jp/techacademy/eri/takashima/autoslideshowappt/MainActivity.java
@@ -35,12 +35,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (Build)
-
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                getContentsInfo();
+            } else {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+            }
+        } else {
+            getContentsInfo();
+        }
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch(requestCode) {
+            case PERMISSIONS_REQUEST_CODE:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    getContentsInfo();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+private void getContentsInfo() {
+    ContentResolver resolver = getContentResolver();
+    Cursor cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+    );
+    if(cursor.moveToFirst()) {
+        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+        Long id = cursor.getLong(fieldIndex);
+        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-
-
+        ImageView imageVIew =(ImageView) findViewById(R.id.imageView1);
+        imageVIew.setImageURI(imageUri);
+    }
+    cursor.close();
+}
 
 }
